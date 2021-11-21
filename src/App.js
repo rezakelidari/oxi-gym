5e7a54be0d0e8f33d8cc47bc41c8df1bded64ab9
Reza Alone <reza.phoenix.1382@gmail.com>
2021-11-21 22:11:41 +0330

    Adding Navbar and Home and NotFound to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="appMain">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
