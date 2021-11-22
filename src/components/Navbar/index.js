e78aae4730c4ef20d90908e359fdcc6e69003eba
Reza Alone <reza.phoenix.1382@gmail.com>
2021-11-22 23:17:15 +0330

    Updating Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
-import Title from "../Title";
+import React, { useReducer, useState } from "react";
 import Styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+import Title from "../Title";
 
 function Navbar() {
+  const burgerReducer = (state, action) => {
+    return state ? false : true;
+  };
+
   const [navClassName, setNavClassName] = useState(`${Styles.navbarMain}`);
+  const [navBurgerState, dispatch] = useReducer(burgerReducer, false);
 
   window.addEventListener("scroll", () => {
     window.scrollY > 0
@@ -13,9 +19,38 @@ function Navbar() {
 
   return (
     <nav className={navClassName}>
-      <Title />
+      <NavLink name="Trainers" path="/trainers" />
+      <Title link />
+      <NavLink name="Pricing" path="/pricing" />
+
+      <span
+        className={`${Styles.navBurger} ${navBurgerState ? Styles.opened : ""}`}
+        onClick={dispatch}
+      ></span>
+      <ul
+        className={`${Styles.navDropDown} ${navBurgerState ? Styles.show : ""}`}
+      >
+        <li>
+          <Link to="/trainers" onClick={() => window.scrollTo(0, 0)}>
+            Trainers
+          </Link>
+        </li>
+        <li>
+          <Link to="/pricing" onClick={() => window.scrollTo(0, 0)}>
+            Pricing
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
+
+  function NavLink({ name, path }) {
+    return (
+      <Link to={path} onClick={() => window.scrollTo(0, 0)}>
+        <div className={Styles.navLink}>{name}</div>
+      </Link>
+    );
+  }
 }
 
 export default Navbar;
