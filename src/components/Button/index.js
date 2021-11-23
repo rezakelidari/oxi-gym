6d06ab1dc8d6a2f79b29b4c49295e3d0464c6189
Reza Alone <reza.phoenix.1382@gmail.com>
2021-11-23 21:26:39 +0330

    Updating Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Styles from "./Button.module.css";
 
-function Button({ children }) {
-  return <button className={Styles.buttonMain}>{children}</button>;
+function Button({ children, path }) {
+  return (
+    <Link to={path ?? ""}>
+      <button
+        className={Styles.buttonMain}
+        onClick={() => path && window.scrollTo(0, 0)}
+      >
+        {children}
+      </button>
+    </Link>
+  );
 }
 
 export default Button;
